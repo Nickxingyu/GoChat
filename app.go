ca7d3e439ad26adfe9456e80b9b93c5085ea0d65
Hero <nick19960723@gmail.com>
2023-12-06 03:55:14 +0800

    fix: debug

--- app.go
+++ app.go
@@ -6,13 +6,13 @@ import (
 	"net/http"
 )
 
-var addr = flag.String("addr", ":8080", "http server address")
+var addr = flag.String("addr", ":3000", "http server address")
 
 func main() {
 	flag.Parse()
 
 	wsServer := GetWsServer()
-	wsServer.Run()
+	go wsServer.Run()
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		wsServer.ServeWs(w, r)
 	})
